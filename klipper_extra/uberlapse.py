e2e60dadef3198f230efde9af2a317445eae3a36
HelgeKeck <helgekeck@hotmail.com>
2021-12-19 18:16:01 +0100

    commit

--- klipper_extra/uberlapse.py
+++ klipper_extra/uberlapse.py
@@ -1,23 +1,39 @@
-import logging
 import subprocess
+import logging
 
 class Uberlapse:
+
     def __init__(self, config):
+        self.name = config.get_name().split()[-1]
         self.printer = config.get_printer()
         self.gcode = self.printer.lookup_object('gcode')
+        self.timeout = config.getfloat('timeout', 2., above=0.)
+        self.url = "http://192.168.0.199:9090"
+        self.endpoint = ""
         self.gcode.register_command('UL_SNAPSHOT', self.cmd_UL_SNAPSHOT, desc=("Calls the uberlapse server to take a image"))
+        self.gcode.register_command('UL_RENDER', self.cmd_UL_RENDER, desc=("Calls the uberlapse server to start the timelapse rendering"))
+        self.gcode.register_command('UL_RESET', self.cmd_UL_RESET, desc=("Calls the uberlapse server to reset it"))
 
-    def cmd_UL_SNAPSHOT(self, gcmd):
+    def cmd_UL_SNAPSHOT(self, param):
+        self.endpoint = "/capture"
+        self._call_URL(self)
+
+    def cmd_UL_RENDER(self, param):
+        self.endpoint = "/render"
+        self._call_URL(self)
+
+    def cmd_UL_RESET(self, param):
+        self.endpoint = "/reset"
+        self._call_URL(self)
+
+    def _call_URL(self, param):
         reactor = self.printer.get_reactor()
         try:
-            proc = subprocess.Popen("wget http://192.168.0.199:9090/capture", shell=True)
+            proc = subprocess.Popen("wget " + self.url + self.endpoint, shell=True)
         except Exception:
-            logging.exception("shell_command: Command {%s} failed" % (self.name))
-            raise self.gcode.error("Error running command {%s}" % (self.name))
-        if self.verbose:
-            self.proc_fd = proc.stdout.fileno()
-            self.gcode.respond_info("Running Command {%s}...:" % (self.name))
-            hdl = reactor.register_fd(self.proc_fd, self._process_output)
+            logging.exception("Uberlapse: Command {%s} failed" % (self.name))
+            raise self.gcode.error("Error calling url {%s}" % (self.url + self.endpoint))
+
         eventtime = reactor.monotonic()
         endtime = eventtime + self.timeout
         complete = False
@@ -28,19 +44,9 @@ class Uberlapse:
                 break
         if not complete:
             proc.terminate()
-        if self.verbose:
-            if self.partial_output:
-                self.gcode.respond_info(self.partial_output)
-                self.partial_output = ""
-            if complete:
-                msg = "Command {%s} finished\n" % (self.name)
-            else:
-                msg = "Command {%s} timed out" % (self.name)
-            self.gcode.respond_info(msg)
-            reactor.unregister_fd(hdl)
-            self.proc_fd = None
+
         self.gcode.respond_info('Ok\n')
-            
+
 
 def load_config(config):
     return Uberlapse(config)
